adfa591062fc1d2a8b1ce3750925c0309a7d9b20
“kim” <kimbfsd@gmail.com>
2020-03-12 05:05:57 +0200

    Updates

---
+++ precache-manifest.3170a89addc5f793308a439e9dd301c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdeb39211de96a97b5a90fc178d610e9",
+    "url": "/globs-app/index.html"
+  },
+  {
+    "revision": "3f7f23611c33aa58a7a7",
+    "url": "/globs-app/static/css/main.878219f3.chunk.css"
+  },
+  {
+    "revision": "d088a9230355287767d5",
+    "url": "/globs-app/static/js/2.9ce390ea.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/globs-app/static/js/2.9ce390ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f7f23611c33aa58a7a7",
+    "url": "/globs-app/static/js/main.76b445e6.chunk.js"
+  },
+  {
+    "revision": "a34404f271a47f5e6a43",
+    "url": "/globs-app/static/js/runtime-main.a915b3f5.js"
+  },
+  {
+    "revision": "c0c24617806a632dc0f4d29badb08015",
+    "url": "/globs-app/static/media/check-grey.c0c24617.svg"
+  },
+  {
+    "revision": "bd68e58eda89f50c9fd568cd4d05f9e5",
+    "url": "/globs-app/static/media/check-red.bd68e58e.svg"
+  }
+]);
